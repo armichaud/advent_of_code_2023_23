ae8a50fc4344e25d920da8a9feee6703c65610eb
armichaud <email@alexandermichaud.tech>
2023-12-23 23:36:03 -0500

    Consts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::{io::{BufReader, BufRead}, fs::File, collections::HashSet};
 use nalgebra::DMatrix;
 
+const FOREST: char = '#';
+const PATH: char = '.';
+const SLOPE_UP: char = '^';
+const SLOPE_DOWN: char = 'v';
+const SLOPE_LEFT: char = '<';
+const SLOPE_RIGHT: char = '>';
+
 fn get_matrix(file: &str) -> DMatrix<char> {
     let file = File::open(file).unwrap();
     let lines = BufReader::new(file).lines();
@@ -16,7 +23,7 @@ fn get_matrix(file: &str) -> DMatrix<char> {
 fn get_start(matrix: &DMatrix<char>) -> Option<(usize, usize)> {
     let first_row = matrix.row(0);
     for col in 0..matrix.ncols() {
-        if first_row[col] == '*' {
+        if first_row[col] == PATH {
             return Some((0, col));
         }
     }
