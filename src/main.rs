d9d9695cb9246f5fe023d7df50cd3771e538665d
armichaud <email@alexandermichaud.tech>
2023-12-23 23:57:56 -0500

    Well it's not working but it's all there

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,57 @@ fn get_start(matrix: &DMatrix<char>) -> Option<(usize, usize)> {
     None
 }
 
-fn longest_path(matrix: &DMatrix<char>, start: (usize, usize), visited: HashSet<(usize, usize)>) -> Option<usize> {
-    if start.0 == matrix.nrows() - 1 {
+fn longest_path(matrix: &DMatrix<char>, current: (usize, usize), visited: HashSet<(usize, usize)>) -> Option<usize> {
+    if current.0 == matrix.nrows() - 1 {
         return Some(0);
     }
-    // TODO
+    let mut visited = visited;
+    visited.insert(current);
+    match matrix[current] {
+        SLOPE_UP => {
+            if let Some(n) = longest_path(matrix, (current.0 - 1, current.1), visited.clone()) {
+                return Some(n + 1);
+            }
+        }
+        SLOPE_DOWN => {
+            if let Some(n) = longest_path(matrix, (current.0 + 1, current.1), visited.clone()) {
+                return Some(n + 1);
+            }
+        }
+        SLOPE_LEFT => {
+            if let Some(n) = longest_path(matrix, (current.0, current.1 - 1), visited.clone()) {
+                return Some(n + 1);
+            }
+        }
+        SLOPE_RIGHT => {
+            if let Some(n) =longest_path(matrix, (current.0, current.1 + 1), visited.clone()) {
+                return Some(n + 1);
+            }
+        }
+        _ => {
+            let neighbors: &[(i32, i32); 4] = &[
+                (current.0 as i32 - 1, current.1 as i32 ), 
+                (current.0 as i32 + 1, current.1 as i32), 
+                (current.0 as i32, current.1 as i32 - 1), 
+                (current.0 as i32, current.1 as i32 + 1)
+            ];
+            let mut longest = 0;
+            for neighbor in neighbors {
+                let neighbor_usize = (neighbor.0 as usize, neighbor.1 as usize);
+                if neighbor.0 > -1 && 
+                    neighbor_usize.0 < matrix.nrows() && 
+                    neighbor.1 > -1 && 
+                    neighbor_usize.1 < matrix.ncols() && 
+                    matrix[neighbor_usize] != FOREST && 
+                    !visited.contains(&neighbor_usize) {
+                        longest = longest.max(longest_path(matrix, neighbor_usize, visited.clone()).unwrap_or(0));
+                    }
+            }
+            if longest > 0 {
+                return Some(longest + 1)
+            }
+        }
+    }
     None
 }
 
